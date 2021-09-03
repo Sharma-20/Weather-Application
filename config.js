b7dd0cec3ba762d488fd0b5b99b4d38d29b97c4d
Saumya <saumya303bajpai@gmail.com>
2021-09-03 14:56:57 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "7ecc1e09c9e64ab47725740c3c1116a6"
+    }
+}
+
+module.exports = constants;
